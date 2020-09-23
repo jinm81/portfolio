dfcbad2af605690369b753ec5647eee18fb3cf5c
hojin Mun <jin1281@gmail.com>
2020-09-22 22:26:57 -0400

    about

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
 import Header from "./Header";
-import contact from "./Contact";
+import Contact from "./Contact";
 import data from "./yourata";
 
 const About = () => {
-    return (
-    <Header name={data.name} contactEmail={data.contactEmail}></Header>
-    <div className="about_container">
+  return (
+    <div>
+      <Header name={data.name} contactEmail={data.contactEmail}></Header>
+      <div className="about_container">
         <h1>{data.aboutTitle}</h1>
         <p>{data.aboutPara}</p>
-
-
-
-
-
-
-
-
+        <div className="about_contact">
+          <Contact
+            contactEmail={data.contactEmail}
+            contactPara={data.contactPara}
+            socialLinks={data.social}
+          ></Contact>
+        </div>
+      </div>
     </div>
-
-
-}
+  );
+};
+export default About;
