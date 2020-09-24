57e3653d2aa65e8170021760138d82193affd4b5
hojin Mun <jin1281@gmail.com>
2020-09-23 21:38:08 -0400

    app.css

---
+++ src/components/yourdata.js
@@ -0,0 +1,14 @@
+export default {
+    
+  
+    //Change The Website Template
+    name: "HojinMun",
+  
+    //Contact Email
+    contactEmail: "hojinmun@gmail.com",
+  
+    // Landing Page Name
+    landingPageName: "Hojinmun",
+  
+    // Landing Page Paragraph
+   
