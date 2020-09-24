413443661408bf3ca59147e58852708e0fa543c2
hojin Mun <jin1281@gmail.com>
2020-09-23 23:29:32 -0400

    spelling errors

--- src/components/yourdata.js
+++ src/components/yourdata.js
@@ -11,4 +11,51 @@ export default {
     landingPageName: "Hojinmun",
   
     // Landing Page Paragraph
-   
+    landingPagePara:
+      "Full stack web developer ...",
+  
+    // Landing Page Image (replace - root/public/assets/photo.jpg)
+    landingPageImage: "assets/image.png",
+  
+    // Change Projects Here
+    projects: [
+      {
+        id: 1, 
+        title: "Trace", 
+        
+        imageSrc: "https://images.pexels.com/photos/4246238/pexels-photo-4246238.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        
+        url: "https://tracerx.herokuapp.com/"
+      },
+      {
+        id: 2, 
+        title: "Employment Management",
+        imageSrc: "https://via.placeholder.com/300",
+        url: "https://jinm81.github.io/employeemanagment"
+      },
+      {
+        id: 3, 
+        title: "Project Three",
+        imageSrc: "https://via.placeholder.com/300",
+        url: ""
+      }
+    ], 
+    // Contact Section
+    contactPara:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    social: [
+      
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/hojinmun/"
+      },
+      {
+        name: "Github",
+        url: "https://www.behance.net/jinm81"
+      },
+         ],
+  
+    // About Page
+    aboutTitle: "develope web page",
+    aboutPara:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text eversince the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  };
